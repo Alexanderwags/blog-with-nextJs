091b086cb0b8ddab8b009ebc6ca433be135984c5
william <alexander171296@gmail.com>
2020-12-12 22:05:31 -0400

    add environment variables

---
+++ test-blog/pages/about.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About | {process.env.SITE_NAME}</title>
+      </Head>
+      <main>
+        <h1>About us</h1>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At culpa quod
+          amet praesentium sit cupiditate corporis aliquid! Commodi cumque, hic
+          reprehenderit vel necessitatibus ut laboriosam a maiores tenetur,
+          optio saepe.
+        </span>
+      </main>
+    </div>
+  );
+}
