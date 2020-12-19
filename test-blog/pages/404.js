b4ec60169ddfd98ad9bd13e116cb011af5650b3f
william <alexander171296@gmail.com>
2020-12-19 16:37:05 -0400

    add page 404

---
+++ test-blog/pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="ed-grid s-py-4">
+      <h1 className="">404 </h1>
+      <span>page request not found</span>
+    </div>
+  )
+}
