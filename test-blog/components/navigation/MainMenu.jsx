4484a7e538897ca8e71c8baddc421f8ff0ebc54e
william <alexander171296@gmail.com>
2020-12-19 16:53:00 -0400

    add reference to MainMenu.jsx

--- test-blog/components/navigation/MainMenu.jsx
+++ test-blog/components/navigation/MainMenu.jsx
@@ -1,11 +1,15 @@
-import React from "react"
+import React, { createRef } from "react"
 import PropTypes from "prop-types"
 import Link from "next/link"
 
+const menu = createRef()
+const toggleMenu = () => {
+  menu.current.classList.toggle("show")
+}
 function MainMenu(props) {
   return (
     <div className="s-grid-1 lg-cols-3 s-cross-center s-main-end header-links ">
-      <nav className="main-menu">
+      <nav className="main-menu" ref={menu}>
         <ul>
           <li>
             <Link href="/">Home</Link>
@@ -18,6 +22,9 @@ function MainMenu(props) {
           </li>
         </ul>
       </nav>
+      <div
+        className="main-menu-toggle tol-l"
+        onClick={() => toggleMenu()}></div>
     </div>
   )
 }
